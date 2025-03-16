13c43be2f64311810eebd78f2fdcdf8d8e099db1
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-03-16 14:41:07 -0300

    Validando login utilizando eloquent para comunicação com banco de dados

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -12,8 +14,16 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function login(Request $request)
+    public function login()
     {
-        dd($request->all());
+        $user = User::query()->where('email', '=', request()->email)->first();
+        $corretPassword = Hash::check(request()->password, $user->password);
+
+        if ($user && $corretPassword) {
+            auth()->login($user);
+            return redirect()->route('dashboard');
+        }
+
+        return back()->with('error', 'Usuário ou senha inválidos!');
     }
 }
--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,1 +1,10 @@
-<h1>Login View</h1>
+<div>
+    <h1>Login</h1>
+
+    <form action="/login" method="post">
+        @csrf
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Senha">
+        <button type="submit">Logar</button>
+    </form>
+</div>
